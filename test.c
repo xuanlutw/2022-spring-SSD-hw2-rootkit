1b14646fb3e5ad721e39e55ad8515e2c909c5888
xuan <s986309@gmail.com>
2022-03-13 02:33:40 +0800

    Add test script.

--- test.c
+++ test.c
@@ -1,24 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include "rootkit.h"
 
-struct masq_proc masq[2] = {
-    {.orig_name = "-bash",
-        .new_name  = "-fish"},
-    {.orig_name = "/sbin/init",
-        .new_name  = "QQAAQQQ"},
-};
-
-struct masq_proc_req masq_req = {
-    .len = 2,
-    .list = masq,
-};
-
-int main (void) {
+int main (int argc, char *argv[]) {
     int fd = open("/dev/rootkit", O_RDWR);
+    long i;
+    struct masq_proc_req masq_req;
 
-    // ioctl(fd, IOCTL_MOD_HIDE);
-    // ioctl(fd, IOCTL_MOD_MASQ, &masq_req);
-    ioctl(fd, IOCTL_MOD_HOOK);
+    if (fd == -1) {
+        fprintf(stderr, "Open rootkit fail!\n");
+        return -1;
+    }
+    if (argc != 2) {
+        fprintf(stderr, "Wrong number of arguments!\n");
+        return -1;
+    }
+
+    if (!strcmp(argv[1], "HIDE")) {
+        ioctl(fd, IOCTL_MOD_HIDE);
+    }
+    else if (!strcmp(argv[1], "HOOK")) {
+        ioctl(fd, IOCTL_MOD_HOOK);
+    }
+    else if (!strcmp(argv[1], "MASQ")) {
+        scanf("%ld", &(masq_req.len));
+        masq_req.list = malloc(sizeof(struct masq_proc) * masq_req.len);
+        if (masq_req.list == NULL) {
+            fprintf(stderr, "malloc fail!\n");
+            return -1;
+        }
+        for (i = 0; i < masq_req.len; ++i)
+            scanf("%s %s", masq_req.list[i].orig_name, masq_req.list[i].new_name);
+        ioctl(fd, IOCTL_MOD_MASQ, &masq_req);
+    }
+    else {
+        fprintf(stderr, "Unknown operation.\n");
+        return -1;
+    }
+    return 0;
 }
---
+++ test.py
@@ -0,0 +1,57 @@
+import os
+
+def test (condition):
+    if condition:
+        print("PASS")
+    else:
+        print("FAIL")
+
+print("===== Install Module =====")
+
+os.system("rmmod rootkit")
+os.system("rm /dev/rootkit")
+
+os.system("insmod rootkit.ko")
+major = int(os.popen("dmesg").read().split("\n")[-7][-4:])
+print(f"major = {major}")
+os.system(f"mknod /dev/rootkit c {major} 0")
+
+
+print("===== Test HIDE =====")
+
+os.system("./test HIDE")
+test("rootkit" not in os.popen("lsmod").read())
+
+os.system("./test HIDE")
+test("rootkit" in os.popen("lsmod").read())
+
+os.system("./test HIDE")
+test("rootkit" not in os.popen("lsmod").read())
+
+os.system("./test HIDE")
+test("rootkit" in os.popen("lsmod").read())
+
+
+print("===== Test MASQ =====")
+
+os.system("./test MASQ < MASQ.in.1")
+log = os.popen("ps aux").read().split("\n")
+test("QQAAQQ" == log[1][-6:]) # No extrat char after it
+
+print("===== Test HOOK =====")
+
+os.system("./test HOOK")
+os.system("ls > /dev/null")
+log = os.popen("dmesg").read().split("\n")
+test("exec /bin/sh"    in log[-5])
+test("exec /bin/ls"    in log[-4])
+test("exec /bin/sh"    in log[-3])
+test("exec /bin/dmesg" in log[-2])
+
+# TODO: How to test poweroff @@
+
+# exec should work after removed the rootkitmodule
+os.system("rmmod rootkit")
+os.system("rm /dev/rootkit")
+os.system("ls > /dev/null")
+test(True)
